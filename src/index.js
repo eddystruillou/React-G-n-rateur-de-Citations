e90b90e44e717cfa953a15609a2590f55f766384
Eddy Struillou <eddy.s25@hotmail.fr>
2018-05-04 15:41:44 +0200

    Mise en place du state + génération des citations en radom

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
+import citations from './citations';
 import './index.css';
 
 class App extends React.Component {
+
+  state = {
+    citation: 'Bienvenue dans ce générateur de citations réalisé avec React',
+    auteur: 'Eddy'
+  };
+
+  genererCitation = event => {
+    //Transformation des citations en array
+    const keyArray = Object.keys(citations);
+    //On génère une citation au hasard
+    const randomKey = keyArray[Math.floor(Math.random() * keyArray.length)];
+    //On récupère la citation dans notre State
+    this.setState(citations[randomKey]);
+  };
+
   render() {
     return (
       <div>
         <p>
-          Ma citation
-          <span>- Auteur</span>
+          {this.state.citation}
+          <span>- {this.state.auteur}</span>
         </p>
-        <button>Une autre citation!</button>
+        <button onClick ={e => this.genererCitation(e)} >Générer</button>
       </div>
     )
   }
