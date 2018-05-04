f2a72ce593b613c55b1f25aecc27592e15380af2
Eddy Struillou <eddy.s25@hotmail.fr>
2018-05-04 15:57:11 +0200

    Boucle de vérif de citation + genererCitation() au start

--- src/index.js
+++ src/index.js
@@ -5,16 +5,22 @@ import './index.css';
 
 class App extends React.Component {
 
-  state = {
-    citation: 'Bienvenue dans ce générateur de citations réalisé avec React',
-    auteur: 'Eddy'
-  };
+  state = {};
+  
+  componentWillMount() {
+    this.genererCitation()
+  }
 
   genererCitation = event => {
     //Transformation des citations en array
     const keyArray = Object.keys(citations);
     //On génère une citation au hasard
     const randomKey = keyArray[Math.floor(Math.random() * keyArray.length)];
+    //On empêche d'avoir 2 fois la même citation
+    if(this.state.citation === citations[randomKey].citation) {
+      this.genererCitation();
+      return;
+    }
     //On récupère la citation dans notre State
     this.setState(citations[randomKey]);
   };
