f27ae709c18cdd3d0637729a1e31e35ae8ecdc0a
Eddy Struillou <eddy.s25@hotmail.fr>
2018-05-04 15:17:47 +0200

    Base + first component

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Ma citation
+          <span>- Auteur</span>
+        </p>
+        <button>Une autre citation!</button>
+      </div>
+    )
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('root')
+);
